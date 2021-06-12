301a4b620f5d8116caa201e9b2786ff97f2b1ad8
Sam Hollings <{31413973}+{Naawww}@users.noreply.github.com>
2021-06-12 12:05:26 +0100

    managed to get command line to display a simple screen with field overlay.

--- main.py
+++ main.py
@@ -3,9 +3,44 @@ This is based on the video by "one lonely coder" - https://www.youtube.com/watch
 and rewritten in Python.
 Author: Sam Hollings"""
 
+import textwrap
+from curses import wrapper
+
 # Assets
 
-# create tetromino class
+def indexer_rotator(x,y, w=4, rotation=0):
+    if rotation == 0:
+        i = x*w + y
+    elif rotation == 1: # 90 degrees
+        i = 12+y-(x*4)
+    elif rotation == 1:  # 90 degrees
+        i = 12 + y - (x * 4)
+    elif rotation == 2: # 180 degrees
+        i = 15 - (y * 4)- x
+    elif rotation == 3: # 270 degrees
+        i = 3 + y + (x * 4)
+    return i
+
+# field definition
+
+SCREEN_WIDTH = 80 # nScreenWidth
+SCREEN_HEIGHT = 30 # nScreenHeight
+FIELD_WIDTH = 12 # nFieldWidth
+FIELD_HEIGHT = 18 # nFieldHeight
+FIELD = '' #pField
+
+# initialise the array
+ACTIVE_FIELD = [0]*(FIELD_WIDTH*FIELD_HEIGHT) # Create play field buffer
+for x in range(0,  FIELD_WIDTH): # Board Boundary
+    for y in range (0, FIELD_HEIGHT):
+        if x == 0 or x == FIELD_WIDTH - 1 or y == FIELD_HEIGHT - 1:
+            cell_value = 9
+        else:
+            cell_value = 0
+        ACTIVE_FIELD[indexer_rotator(x, y, w=FIELD_WIDTH)] = cell_value
+
+
+# create tetromino object
 
 tetromino = []
 
@@ -51,15 +86,48 @@ tetromino.append("...." \
                  ".X.." \
                  ".X..")
 
-def indexer_2d_to_1d(x,y, w=4, rotation=0):
-    if rotation == 0:
-        i = x*w + y
-    elif rotation == 1: # 90 degrees
-        i = 12+y-(x*4)
-    elif rotation == 1:  # 90 degrees
-        i = 12 + y - (x * 4)
-    elif rotation == 2: # 180 degrees
-        i = 15 - (y * 4)- x
-    elif rotation == 3: # 270 degrees
-        i = 3 + y + (x * 4)
-    return i
+
+# Display
+
+# Draw the field
+
+## testing
+FIELD_WIDTH = 4
+FIELD_HEIGHT = 4
+
+SCREEN_WIDTH = 10
+SCREEN_HEIGHT = 10
+# end test
+
+screen = ['.']*(SCREEN_WIDTH*SCREEN_HEIGHT)
+offset = 2
+
+for x in range(0, FIELD_WIDTH):
+    for y in range(0, FIELD_HEIGHT):
+        screen[((y + offset) * SCREEN_WIDTH) + (x + offset)] = " ABCDEFG=#"[[9,9,9,9,9,0,0,9,9,0,0,9,9,9,9,9][y * FIELD_WIDTH + x]];
+        #screen[(y + offset) * SCREEN_WIDTH + (x + offset)] = " ABCDEFG=#"[ACTIVE_FIELD[y * FIELD_WIDTH + x]];
+
+def wrap(string, max_width):
+    s=''
+    for i in range(0,len(string),max_width):
+        s=s+string[i:i+max_width]
+        s=s+'\n'
+    return s
+
+#print(wrap("".join(screen),SCREEN_WIDTH))
+#
+# exit()
+
+# for y, row in enumerate(wrap("".join(screen),SCREEN_WIDTH)):
+#     print(row)
+
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
+
+    stdscr.addstr(0, 0, wrap("".join(screen),SCREEN_WIDTH))
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+wrapper(main)
