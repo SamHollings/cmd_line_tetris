98b77720c2e3350c392856781b21219df8e32a93
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2021-06-12 09:04:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+""" Command Line Tetris Python application
+This is based on the video by "one lonely coder" - https://www.youtube.com/watch?v=8OK8_tHeCIA&t=140s
+and rewritten in Python.
+Author: Sam Hollings"""
+
+# Assets
+
+# create tetromino class
+
+tetromino = []
+
+tetromino[0].append()
+
